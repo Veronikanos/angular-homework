c5f531702e3c9645d12c00c327fbc44aca09dcec
Veronica <vveronikanika@gmail.com>
2023-04-30 20:01:43 +0300

    feat: add toolbar component, add buttons and inputs for interaction

--- homework-part1/src/app/app.module.ts
+++ homework-part1/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { UsersModule } from './users/users.module';
+// import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, UsersModule],
+
   providers: [],
   bootstrap: [AppComponent],
 })
--- homework-part1/src/app/users/users.module.ts
+++ homework-part1/src/app/users/users.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
-import { UsersRoutingModule } from '../users-routing.module';
-import { UserToolbarComponent } from './user-toolbar/user-toolbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
+import { UsersComponent } from './users.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+// import { UsersComponent } from './users.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+// import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { MatSelectModule } from '@angular/material/select';
+// import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatTableModule } from '@angular/material/table';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  FormsModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  // MatSortModule,
+  // MatExpansionModule,
+  // MatTableModule,
+];
 
 @NgModule({
-  declarations: [UserToolbarComponent],
-  imports: [
-    CommonModule,
-    UsersRoutingModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatFormFieldModule,
-  ],
+  declarations: [ToolbarComponent, UsersComponent],
+  imports: [CommonModule, MaterialComponents],
+  exports: [MaterialComponents, ToolbarComponent, UsersComponent],
 })
 export class UsersModule {}
